708abcd3a1c85ed29bb952d8c340cbcc46586f3a
aashish-patil-work <aashish.patil.work@gmail.com>
2024-08-10 11:36:40 +0530

    log added

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -15,7 +15,7 @@ export default function TodoList({ todoList, setTodoList }) {
 
   //**delete item */
   function handleDelete(itemClicked) {
-    console.log("handleDelete ", itemClicked);
+    console.log("handleDelete log: ", itemClicked);
     setTodoList(todoList.filter((item) => item !== itemClicked));
   }
 
